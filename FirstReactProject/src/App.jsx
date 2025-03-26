d1dcc73642eede0f07fb93e2015993670ad98555
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-26 23:23:38 +0530

    update

--- FirstReactProject/src/App.jsx
+++ FirstReactProject/src/App.jsx
@@ -1,9 +1,9 @@
 
 let App=()=>{
   return(
-    <h1>Hello World,This is my first React project!</h1>
-
+    <h1>Hello World, <br />This is my first React project!</h1>
   )
 }
 
 export default App;
+
