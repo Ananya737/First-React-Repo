4962f67e4fc2bfc9afdbc495ae228a038bf6479f
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-27 13:22:36 +0530

    update

--- FirstReactProject/src/App.jsx
+++ FirstReactProject/src/App.jsx
@@ -1,7 +1,7 @@
 
 let App=()=>{
   return(
-    <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "Arial" }}>
+    <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "Arial",color:"red" }}>
     <h1 >Hello World, <br />This is my first React project!</h1>
     </div>
   )
