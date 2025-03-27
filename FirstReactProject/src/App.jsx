84d8a9c702a6a22da595a62dba5ed0add7a1e852
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-27 17:11:24 +0530

    update

--- FirstReactProject/src/App.jsx
+++ FirstReactProject/src/App.jsx
@@ -1,8 +1,15 @@
 
+const name="Ananya";
+const age=22;
+const lpa=7;
+
+
+
 let App=()=>{
   return(
-    <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "Arial",color:"red" }}>
-    <h1 >Hello World, <br />This is my first React project!</h1>
+    <div>
+    <h1 >Hello World,I am {name}! <br /> </h1>
+    <h3>My age is {age} <br /> My salary is {lpa*2} LPA.</h3>
     </div>
   )
 }
