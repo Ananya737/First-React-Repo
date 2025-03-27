48edb5067464027d2b3f82ca555a0c224459f1ef
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-27 23:13:13 +0530

    update

--- FirstReactProject/src/App.jsx
+++ FirstReactProject/src/App.jsx
@@ -3,20 +3,35 @@
 // const age=22;
 // const lpa=7;
 
-// const table1=<table>
-//   <thead>
-//     <tr>Author</tr>
-//   </thead>
-//   <tbody>
-//     <tr>
-//       <td>Ruskin Bond</td>
-//     </tr>
-//   </tbody>
-// </table>
+const table1=<table border={2}>
+  <thead>
+    <tr>
+      <td>Sr. No.</td>
+      <td>Author</td>
+      <td>Book</td>
+    </tr>
+    
+  </thead>
+  <tbody>
+    <tr>
+      <td>01</td>
+      <td>Ruskin Bond</td>
+      <td>The Whistling School Boy</td>
+    </tr>
+    <tr>
+      <td>02</td>
+      <td>Preety Shenoy</td>
+      <td>Life is what you make it</td>
+    </tr>
+  </tbody>
+</table>
 
 
 let App=()=>{
   return(
+
+    <h3>{table1}</h3>
+
 //     <div>
 //  <h1>Hello</h1>
 //  <h1>Hello</h1>
@@ -24,12 +39,15 @@ let App=()=>{
 //  <h1>Hello</h1>
 //  </div>
 
-    <>
- <h1>Hello</h1>
- <h1>Hello</h1>
- <h1>Hello</h1>
- <h1>Hello</h1>
- </>
+
+// Fragment tag 
+ 
+//     <>
+//  <h1>Hello</h1>
+//  <h1>Hello</h1>
+//  <h1>Hello</h1>
+//  <h1>Hello</h1>
+//  </>
   )
 }
 
