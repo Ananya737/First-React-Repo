51ed071381772faba47a974049632e351336b54c
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-29 00:16:34 +0530

    update

--- FirstReactProject/src/App.jsx
+++ FirstReactProject/src/App.jsx
@@ -2,16 +2,16 @@ import Header from "./Header";
 import Data from "./Data";
 import Footer from "./Footer";
 
-import { Header1,Data1,Footer1 } from "./Website";
+
 
 
 
 const App=()=>{
   return(
     <>
-    <Header1/>
-    <Data1/>
-    <Footer1/>
+    <Header/>
+    <Data/>
+    <Footer/>
 
 
 
