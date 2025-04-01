33ca0bf076deaecdbacf0f42cbc6c968ed617a86
Ananya Sahu <ananyasahu59513@gmail.com>
2025-04-02 00:05:55 +0530

    Navigation with Error page

--- FirstReactProject/src/App.jsx
+++ FirstReactProject/src/App.jsx
@@ -4,6 +4,9 @@ import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
 import Services from "./Services";
+import Error from "./Error";
+
+
 
 const App=()=>{
 return(
@@ -11,10 +14,12 @@ return(
 <BrowserRouter>
 <Routes>
   <Route path="/" element={<Layout/>}>
+  <Route index element={<Home/>}/>
   <Route path="home" element={<Home/>}/>
   <Route path="about" element={<About/>}/>
   <Route path="contact" element={<Contact/>}/>
   <Route path="services" element={<Services/>}/>
+  <Route path="*" element={<Error/>}/>
   
 
   </Route>
